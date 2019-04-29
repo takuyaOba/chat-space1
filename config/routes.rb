96923671450c48383790810523106cc1ea73db1f
takuyaOba <letztbattalion001@gmail.com>
2019-04-29 21:46:32 +0900

    ok
    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "messages/index" => "messages#index"  
+  get "messages/index" => "messages#index"
+  resources :users, only: [:edit, :update]
 end
