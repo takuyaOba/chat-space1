662fd1389423c1f1f00cb0b10ea18ce40cb7919a
takuyaOba <letztbattalion001@gmail.com>
2019-05-01 15:20:16 +0900

    グループ作成できた

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "messages/index" => "messages#index"
+  root 'messages#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
