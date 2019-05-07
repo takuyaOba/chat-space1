f4fb8cbd122af2024b8a0a08368833cdac730698
takuyaOba <letztbattalion001@gmail.com>
2019-05-07 20:57:27 +0900

    $("form")[0].reset();追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -31,12 +31,13 @@ $(function(){
         processData: false,
         contentType: false
       })
-      .done(function(data){
-        var html = buildHTML(data);
+      .done(function(message){
+        var html = buildHTML(message);
         $('.messages').append(html);
-        $(".form__submit").prop( "disabled", false );
+        $(".form__submit").prop( "disabled", false);
         $("html,body").animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        $('.form__message').val('');
+        $("form")[0].reset();
+
 
       })
       .fail(function(){
