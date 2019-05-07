2fbf1bfd8bb619842d1a9a4351937ad1406ea60e
takuyaOba <letztbattalion001@gmail.com>
2019-05-07 20:20:51 +0900

    animateスクロール

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,10 +4,10 @@ $(function(){
         var html = `<div class=message>
                         <div class="upper-message">
                           <div class="upper-message__user-name">
-                          ${message.user_name}
+                            ${message.user_name}
                           </div>
                           <div class="upper-message__date">
-                          ${message.created_at}
+                            ${message.created_at}
                           </div>
                         </div>
                         <div class="lower-message">
@@ -16,31 +16,31 @@ $(function(){
                           </p>
                           ${imagehtml}
                         </div>
-                      </div> `
+                    </div> `
         return html;
-      }
+  }
   $('#new_message').on('submit', function(e){
-        e.preventDefault();
-        var formData = new FormData(this);
-           var href = window.location.href
-        $.ajax({
-          url: href,
-          type: "POST",
-          data: formData,
-          dataType: 'json',
-          processData: false,
-          contentType: false
-        })
-        .done(function(data){
-                var html = buildHTML(data);
-                $('.messages').append(html);
-               $( ".form__submit").prop( "disabled", false );
-               $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-               $('.form__message').val('');
-               $('.hidden').val('');
-              })
-             .fail(function(){
-                alert('error');
-              })
-            })
-          });
+      e.preventDefault();
+      var formData = new FormData(this);
+      var href = window.location.href
+      $.ajax({
+        url: href,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $(".form__submit").prop( "disabled", false );
+        $("html,body").animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('.form__message').val('');
+
+      })
+      .fail(function(){
+        alert('error');
+      })
+  })
+});
