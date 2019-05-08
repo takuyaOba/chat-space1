89dcb8dc7a9b6cc4695aaa044825186313be982b
takuyaOba <letztbattalion001@gmail.com>
2019-05-08 20:15:18 +0900

    コメントを削除

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -13,7 +13,7 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     var href = window.location.href
-    console.log(input);
+    
     
     $.ajax({
       type: 'GET',
