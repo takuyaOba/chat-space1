ef68096dfd4564ecb6c7131c1a120904536ae5e0
takuyaOba <letztbattalion001@gmail.com>
2019-05-14 12:49:33 +0900

    コメント削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function(){
   $(function(){
     function buildHTML(message){
-      console.log(message);
           var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
       if(message.content && message.image) {
             var html = `<div class="message" data-messageId='${message.id}' data-groupId="${message.group_id}">
@@ -80,25 +79,19 @@ $(document).on('turbolinks:load', function(){
         })
     });
         var reloadMessages = function() {
-          //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
           var last_message_id = $('.message').last().attr("data-messageId");
           var groupId = $('.message').attr("data-groupId");
           $.ajax({
-            //ルーティングで設定した通りのURLを指定
             url: '/groups/' + groupId + '/api/messages',
-            //ルーティングで設定した通りhttpメソッドをgetに指定
             type: 'get',
             dataType: 'json',
-            //dataオプションでリクエストに値を含める
             data: {id: last_message_id}
           })
           .done(function(data) {
-            console.log("done")
             data.forEach(function(message){
             var HTML = buildHTML(message);
               $('.messages').append(HTML);
               $("html,body").animate({scrollTop: $(".messages")[0].scrollHeight+150}, "fast");
-              console.log("done2")
             })
           })
           .fail(function() {
